83123d7056f9ab3fcfcecbca5985c56abf9a128e
Dejan Pavlic <dpavlic@gmail.com>
2022-02-09 20:32:19 -0500

    WIP: Update init.el
    This version includes a switch to straight.el, as well as
    large changes to the overall structure of the configuration.

--- init.el
+++ init.el
@@ -4,21 +4,21 @@
 	 (mapcar #'file-name-as-directory path-vals)))
     (mapconcat 'identity map-to-dirs "")))
 
-;; M-x elpamr-create-mirror-for-installed to install / update local mirror
-(add-to-list 'load-path (my/join-path user-emacs-directory "pkg" "elpa-mirror"))
-(require 'elpa-mirror)
-(setq package-archives `(("myelpa" . ,(my/join-path user-emacs-directory "pkg" "my-elpa"))))
-(require 'use-package)
-(setq use-package-compute-statistics t)
+;; Bootsrap straight.el and use-package
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(defun my-switch-archive(arg)
-  "Switch between MELPA and personal MELPA mirror"
-  (interactive "MELPA (M) or my elpa (any other key): ")
-  (if (string-equal arg "M")
-      (setq package-archives `(("MELPA" . "https://melpa.org/packages/")))
-      (setq package-archives `(("myelpa" . ,(my/join-path u "pkg" "my-elpa"))))
-      )
-  (message "%s" package-archives))
+(straight-use-package 'use-package)
 
 ;; Basic Configuration
 ;; 1. Display Line Numbers
@@ -103,7 +103,7 @@
 ;; Set default fonts
 (use-package
   atom-one-dark-theme
-  :ensure t
+  :straight t
   :config (load-theme 'atom-one-dark t))
 
 (setq column-number-mode t)
@@ -127,61 +127,59 @@
 
 ;; Packages
 (use-package org :defer)
-(use-package hydra :ensure t)
-(use-package major-mode-hydra
-  :ensure t
-  :bind
-  ("M-SPC" . major-mode-hydra))
-(use-package pretty-hydra :ensure t)
-(use-package fontawesome :ensure t)
-(use-package key-chord :ensure t :config (key-chord-mode 1))
-(use-package helm :ensure t)
-(use-package swiper-helm :ensure t)
-(use-package avy :ensure t :config (global-set-key (kbd "C-/") 'avy-goto-char-timer))
-(use-package magit :ensure t)
+(use-package hydra :straight t)
+(use-package major-mode-hydra :straight t)
+(use-package pretty-hydra :straight t)
+(use-package key-chord :straight t :config (key-chord-mode 1))
+(use-package all-the-icons :straight t)
+(use-package helm :straight (helm :commit "08c4ad8e80394c8bc2c0d50429bce765f03826db"))
+(use-package swiper-helm :straight t :after helm)
+(use-package avy :straight t :config (global-set-key (kbd "C-/") 'avy-goto-char-timer))
+(use-package magit :straight t)
+(use-package yaml-mode :straight t :config (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
 
 (use-package
   rainbow-delimiters
-  :ensure t
+  :straight t
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package
   projectile
-  :ensure t)
+  :straight t)
 
-(use-package
-  undo-fu
-  :ensure t
+(use-package undo-fu
+  :straight t
   :config (global-unset-key (kbd "C-z"))
           (global-set-key (kbd "C-z") 'undo-fu-only-undo)
 	  (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
 
 (use-package
   which-key
-  :ensure t
+  :straight t
   :init ;;(which-key-setup-minibuffer)
   (which-key-setup-side-window-bottom)
   (which-key-mode)
   (define-key which-key-mode-map (kbd "C-x \\") 'which-key-C-h-dispatch))
-
-(use-package general :ensure t)
+(use-package general :straight t)
 
 (use-package
   flymake-shellcheck
-  :ensure t
+  :straight t
   :commands flymake-shellcheck-load
   :init (add-hook
 	 'sh-mode-hook
 	 'flymake-shellcheck-load))
 
 ;; Completion Packages
-(use-package eglot :ensure t)
+(use-package eglot :straight t)
 (use-package
   company
-  :ensure t
+  :straight t
   :config (add-hook
 	   'after-init-hook
 	   'global-company-mode)
+  ;; Disable company in eshell
+  (setq company-global-modes '(not emacs-mode))
   ;; Start completing right away, prefix of 2 and wrap selections around
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length
@@ -194,32 +192,53 @@
 ;; Dashboard
 (use-package
   dashboard
-  :ensure t
+  :straight t
   :config (dashboard-setup-startup-hook)
   (setq dashboard-banner-logo-title
 	"Dashboard")
   (setq dashboard-set-footer nil))
 
+;; Hydra functions
 (defun with-faicon (icon str &optional height v-adjust)
-  (s-concat (all-the-icons-faicon icon :v-adjust (or v-adjust 0) :height (or height 1)) " " str))
+  (s-concat (all-the-icons-faicon icon :v-adjust (or v-adjust 0)
+  :height (or height 1)) " " str))
 
+(defmacro my-pretty-hydra-define (name body heads)
+  (let* ((used-keys (->> (--remove (stringp it) heads)
+			 (-flatten-n 1)
+		         (--map (car it))))
+	 (lower-keys '("q" "w" "e" "r" "t" "y" "i" "o" "p" "a" "s" "d" "h" "f" "g" "h"
+		       "j" "k" "l" "z" "x" "c" "v" "n" "m"))
+	 (all-common-keys (append lower-keys (--map (upcase it) lower-keys)))
+	 (all-keys
+	  (append all-common-keys '("1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "="
+				    "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+"
+				    "`" "~" "{" "}" "|" "[" "]" "\\" ";" ":" "'"
+				    "<" ">" "," "." "/" "?")))
+	 (unused-keys (-difference all-keys used-keys))
+	 (unused-definitions (-map (lambda (x) `(,x ignore nil)) unused-keys))
+	 (final-heads (append heads `("" ,unused-definitions))))
+    `(pretty-hydra-define ,name ,body ,final-heads)))
+
+;; Hydra definitions
 (defvar hydra-hydra--title (with-faicon "map" "Hydra" 1 -0.05))
-(defvar hydra-system--title (with-faicon "map" "System" 1 -0.05))
-(defvar hydra-nav--title (with-faicon "map" "Nav" 1 -0.05))
-
-(pretty-hydra-define hydra-hydra (:foreign-keys warn :title hydra-hydra--title :quit-key "q" :exit t)
+(my-pretty-hydra-define hydra-hydra (:color pink :title hydra-hydra--title :quit-key "q")
   ("Hydras"
-   (("s" hydra-system/body "system")
-    ("k" hydra-nav/body "nav"))))
+   (("s" hydra-system/body "system" :exit t)
+    ("k" hydra-nav/body "nav" :exit t)
+    ("w" hydra-window/body "window" :exit t))))
 
-(pretty-hydra-define hydra-system (:foreign-keys warn :title hydra-system--title :quit-key "q")
+(defvar hydra-system--title (with-faicon "map" "System" 1 -0.05))
+(my-pretty-hydra-define hydra-system (:color pink :title hydra-system--title :quit-key "q")
   ("System"
    (("b" helm-buffers-list "buffer list")
+    ("e" eshell "eshell")
     ("s" save-buffer "buffer save")
     ("g" magit-status "git status")
     ("." hydra-hydra/body "hydras" :exit t))))
 
-(pretty-hydra-define hydra-nav (:foreign-keys warn :title hydra-nav--title :quit-key "q")
+(defvar hydra-nav--title (with-faicon "map" "Nav" 1 -0.05))
+(my-pretty-hydra-define hydra-nav (:color pink :title hydra-nav--title :quit-key "q")
   ("Basic"
    (("f" forward-char "forward")
     ("b" backward-char "backward")
@@ -245,8 +264,9 @@
    (("k" kill-line "kill line")
     ("d" delete-char "del char")
     ("D" kill-word "kill word")
-    ("<SPC>" (cond ((not mark-active) (call-interactively 'set-mark-command))
-                   (t (deactivate-mark))) "mark")
+    ("<SPC>" (
+	      cond ((not mark-active) (call-interactively 'set-mark-command)
+		    (t (deactivate-mark))) "mark"))
     ("w" kill-region "kill region")
     ("W" kill-ring-save "save region")
     ("<backspace>" delete-backward-char nil)
@@ -262,14 +282,29 @@
 	 (insert txt)) "insert")
     ("." hydra-hydra/body "hydras" :exit t))))
 
-;; Keybindings
-(general-define-key "M-x" 'helm-M-x)
+(defvar hydra-window--title (with-faicon "windows" "Window Management" 1 -0.05))
+(pretty-hydra-define hydra-window (:color pink :title hydra-window--title :quit-key "q")
+  ("Actions"
+   (("TAB" other-window "switch")
+    ("d" delete-window "delete")
+    ("m" delete-other-windows "maximize")
+    ("s" window-swap-states "swap")
+    ("a" ace-select-window "select"))
 
+   "Resize"
+   (("<left>" shrink-window-horizontally "←")
+    ("<right>" enlarge-window-horizontally "→")
+    ("k" move-border-up "↑")
+    ("l" move-border-right "→")
+    ("n" balance-windows "balance")
+    ("f" toggle-frame-fullscreen "toggle fullscreen"))
+
+   "Split"
+   (("\\" split-window-right "split right")
+    ("-" split-window-below  "split below"))))
+
+;; Keybindings
 (setq key-chord-two-keys-delay 0.4)
-(general-define-key
- :prefix "C-x"
- "b" 'helm-buffers-list
- "C-f" 'helm-find-files)
 (general-define-key
  :prefix "C-c"
  "e" 'eshell
@@ -277,38 +312,11 @@
 (general-define-key
  (general-chord "kj") 'hydra-nav/body
  (general-chord "\\s") 'hydra-system/body
- (general-chord "\\x") 'helm-M-x)
+ (general-chord "\\x") 'helm-M-x
+ (general-chord "\\w") 'hydra-window/body
+ (general-chord "\\b") 'helm-buffers-list)
 
 ;; Set custom variables in a different file
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
 
-;; (defvar jp-window--title (with-faicon "windows" "Window Management" 1 -0.05))
-
-;; (pretty-hydra-define jp-window (:foreign-keys warn :title jp-window--title :quit-key "q")
-;;   ("Actions"
-;;    (("TAB" other-window "switch")
-;;     ("x" ace-delete-window "delete")
-;;     ("m" ace-delete-other-windows "maximize")
-;;     ("s" ace-swap-window "swap")
-;;     ("a" ace-select-window "select"))
-
-;;    "Resize"
-;;    (("h" move-border-left "←")
-;;     ("j" move-border-down "↓")
-;;     ("k" move-border-up "↑")
-;;     ("l" move-border-right "→")
-;;     ("n" balance-windows "balance")
-;;     ("f" toggle-frame-fullscreen "toggle fullscreen"))
-
-;;    "Split"
-;;    (("b" split-window-right "horizontally")
-;;     ("B" split-window-horizontally-instead "horizontally instead")
-;;     ("v" split-window-below "vertically")
-;;     ("V" split-window-vertically-instead "vertically instead"))
-
-;;    "Zoom"
-;;    (("+" zoom-in "in")
-;;     ("=" zoom-in)
-;;     ("-" zoom-out "out")
-;;     ("0" jp-zoom-default "reset"))))
